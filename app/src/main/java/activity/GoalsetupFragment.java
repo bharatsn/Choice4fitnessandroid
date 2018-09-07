d2cfc984fa4f34570d2654027e297e7c94780eb7
Kaopoulos-boss <kaopoulos@gmail.com>
2018-09-07 13:16:46 -0700

    register,forgetpassword layout

--- app/src/main/java/activity/GoalsetupFragment.java
+++ app/src/main/java/activity/GoalsetupFragment.java
@@ -17,7 +17,7 @@ public class GoalsetupFragment extends Fragment {
     TextView optimaltemp,gmedium,oxygenre;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_productdetailtwo, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_goalsetup, container, false);
 
         return rootView;
     }
---
+++ app/src/main/java/com/example/kaopo/choice4fitnessandroid/Signup.java
@@ -0,0 +1,19 @@
+package com.example.kaopo.choice4fitnessandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Signup extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
